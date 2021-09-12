df0967e5d97a563b920a4b6e8310b509960b8aca
Yury <yura.sviderko@live.com>
2021-09-12 10:33:14 +0300

    add stats

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Grid } from 'styled-css-grid';
 import { BackButton, StyledKeyboardButton } from '../components/Buttons';
 import Flexbox from '../components/Flexbox';
 import { useDispatch, useSelector } from 'react-redux';
 import { addAnswerReducer, delAnswerReducer, clsAnswerReducer} from '../store/AnswerReducer';
+
+const StyledStats = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    opacity: 0.7;
+    position: absolute;
+    top: 0.7rem;
+    right: 0.7rem;
+    font-size: 0.9rem;
+    font-weight: 200;
+    div {
+        margin-left: 10px;
+        &:nth-child(1) {
+            color: ${props => props.theme.colors.accent};
+        }
+        &:nth-child(2) {
+            color: #e74747;
+        }
+        &:nth-child(3) {
+            color: #cacaca;
+        }
+    }
+    
+`
 
 const StyledTask = styled.div`
     font-size: 2.7rem;
@@ -30,6 +55,12 @@ const Game = (props) => {
     const task = useSelector(state => state.task.task);
     const mode = useSelector(state => state.task.mode);
 
+    const [stats, setStats] = useState({
+        true: 0,
+        false: 0,
+        accuracy: 0
+    })
+    
     const modes = ['ADDITION', 'SUBSTRACTION', 'MULTIPLICATION', 'DIVISION'];
 
     function onNumber(num) {
@@ -67,8 +98,19 @@ const Game = (props) => {
                 break;
         }
         if(check === +answer) {
+            setStats({
+                true: stats.true + 1,
+                false: stats.false,
+                accuracy: (((stats.true + 1) / (stats.true + 1 + stats.false)).toFixed(3) * 100).toFixed(0) + '%'
+            })
             addTask();
             dispatch(clsAnswerReducer());
+        } else {
+            setStats({
+                true: stats.true,
+                false: stats.false + 1,
+                accuracy: (((stats.true) / (stats.true + stats.false + 1)).toFixed(3) * 100).toFixed(0) + '%'
+            })
         }
     }
 
@@ -77,6 +119,15 @@ const Game = (props) => {
     return (
         <Flexbox direction="column">
             <BackButton/>
+            {
+                stats.accuracy ? (
+                    <StyledStats>
+                        <div>{stats.true}</div>
+                        <div>{stats.false}</div>
+                        <div>{stats.accuracy}</div>
+                    </StyledStats>
+                ) : null
+            }
             <Flexbox direction="column" justify="center" align="center" height="100%">
                 <StyledTask>{task.first} {task.operation === '*' ? '×' : task.operation} {task.second}</StyledTask>
                 <StyledTask>?</StyledTask>
